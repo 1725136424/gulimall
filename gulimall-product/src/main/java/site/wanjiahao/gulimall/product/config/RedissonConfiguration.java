624f0674e48823f63a2ceb0b2210083b187e7eae
wanjiahao <1725136424@qq.com>
2021-03-21 17:23:37 +0800

    修复BUG1.1

--- gulimall-product/src/main/java/site/wanjiahao/gulimall/product/config/RedissonConfiguration.java
+++ gulimall-product/src/main/java/site/wanjiahao/gulimall/product/config/RedissonConfiguration.java
@@ -3,17 +3,21 @@ package site.wanjiahao.gulimall.product.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConfigurationProperties(prefix = "spring.redis")
 public class RedissonConfiguration {
+
+    private String host;
 
     @Bean
     public RedissonClient redisson() {
         // 默认连接地址 127.0.0.1:6379
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://192.168.56.10:6379");
+        config.useSingleServer().setAddress("redis://" + host + ":6379");
         return Redisson.create(config);
     }
 
